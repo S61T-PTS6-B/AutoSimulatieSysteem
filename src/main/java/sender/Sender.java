57ee13323a553d949d569c23258e652b8ba8ff85
i296263 <max95_nl@live.nl>
2016-05-25 14:59:03 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/model/Invoice.java
    #	target/classes/controller/DBServlet.class
    #	target/classes/model/CarOwner_.class
    #	target/classes/model/CarTracker_.class
    #	target/classes/model/Cordon_.class
    #	target/classes/model/Invoice.class
    #	target/classes/model/Invoice_.class
    #	target/classes/model/Location.class
    #	target/classes/model/NAW_.class
    #	target/classes/model/RoadRate_.class
    #	target/classes/model/Road_.class

--- src/main/java/sender/Sender.java
+++ src/main/java/sender/Sender.java
@@ -9,54 +9,32 @@ import com.sun.messaging.ConnectionConfiguration;
 import com.sun.messaging.ConnectionFactory;
 import com.sun.messaging.Queue;
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.jms.*;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import model.Car;
 import model.Location;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  *
  * @author Max
  */
 public class Sender {
-//    private final InitialContext jndiContext;
-//    private final ConnectionFactory connectionFactory;
-//    private final Destination dest;
-//    private final Connection connection;
-//    private final Session session;
-//    private final MessageProducer producer;
-//    private TextMessage message;
     
     public Sender() {
-//        jndiContext = new InitialContext();
-//        connectionFactory = (ConnectionFactory)jndiContext.lookup("jms/__defaultConnectionFactory");
-//        dest = (Destination) jndiContext.lookup("CarSimulator");
-//        connection = connectionFactory.createConnection();
-//        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-//        producer = session.createProducer(dest);
     }
     
     public boolean sendPackage(ArrayList<Location> locations, String carName) throws JMSException {
-//        try {
-////            message = session.createObjectMessage((Serializable) locations.toString());
-////            message.setStringProperty("carName", carName);
-////            message.setObject(locations);
-//            message = session.createTextMessage("Hoi");
-//            message.setStringProperty("carName", carName);
-//            producer.send(message);
-//        } catch (Exception ex) {
-//            System.out.println("Message not sent: " + ex.toString());
-//            return false;
-//        }
-//        return true;
-        
+
         ConnectionFactory connFactory = new ConnectionFactory();
-        connFactory.setProperty(ConnectionConfiguration.imqAddressList, "145.93.81.179:7676");
+        connFactory.setProperty(ConnectionConfiguration.imqAddressList, "145.93.81.93:7676");
 
         Queue myQueue = new Queue("queuevp");
 
@@ -73,8 +51,10 @@ public class Sender {
                 JSONObject jloc = new JSONObject();
                 jloc.put("lat", loc.getLatitude());
                 jloc.put("long", loc.getLongitude());
-                jloc.put("date", loc.getDate());
-                jsArray.add(jloc);
+                DateFormat sdf = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss");
+                String date = sdf.format(new Date());
+                jloc.put("date", date);
+                jsArray.put(jloc);
             }
             container.put("locations", jsArray);
             message.setStringProperty("locations", container.toString());
